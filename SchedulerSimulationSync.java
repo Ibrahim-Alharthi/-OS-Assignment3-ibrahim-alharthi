f99cc7c0661ef0193a1dcf0f081408257cccccdb
اسمك <445050024@std.psau.edu.sa>
2026-04-30 22:52:43 +0300

    adding semaphore to the method run

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -93,7 +93,7 @@ class SharedResources {
       try {
           executionLog.add(message);
       } finally {
-              LogLock.unlock();
+              LogLock.lock();
 
       }
     }
@@ -125,6 +125,7 @@ class Process implements Runnable {
         // This ensures only allowed number of processes run simultaneously
         
         try {
+            SharedResources.CpuSemaphore.acquire();
             if (startTime == -1) {
                 startTime = System.currentTimeMillis();
             }
@@ -183,11 +184,13 @@ class Process implements Runnable {
             }
             System.out.println();
             
-        } finally {
-            // TODO #4: Release CPU semaphore here
-            // Always release in finally block to prevent deadlocks!
-        }
+       } catch (InterruptedException e) {
+        e.printStackTrace();
+    } finally {
+       
+        SharedResources.CpuSemaphore.release();
     }
+}
     
     private String createProgressBar(int progress, int width) {
         int filled = (progress * width) / 100;
